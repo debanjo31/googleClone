c8b87c0d0d0ed28a17ce0fa06619452bca97ba80
debanjo <debanjo31@gmail.com>
2022-07-10 01:07:07 +0100

    'main-commit'

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+      window.onload(alert("Hello "));
+      
+   
+    
+//    //need to create a function that toogle as the input is been clicked between white and black
+      
+//         $("input[type='checkbox']").click(function(e){
+        
+//           if($(this).is(":checked")){  
+//             $("body").get(0).style.setProperty("--white","rgb(255, 255, 255)")
+//             $("body").get(0).style.setProperty("--black","rgb(0, 0, 36)")
+//           }
+//           else if($(this).is(":not(:checked)")){
+//             $("body").get(0).style.setProperty("--white","rgb(255, 255, 255)")
+//             $("body").get(0).style.setProperty("--black","rgb(0, 0, 0)")
+//           }
+          
+//         }
+//         );  
+
+    $(".theme").click(function () {
+        console.log("Hello ");
+    });
+      
+
+  
+    });
+  
